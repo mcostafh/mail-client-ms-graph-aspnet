b8b946cf661676527b8e330c0a796fdb764e41ac
viv-liu <viv.liu@mail.utoronto.ca>
2016-07-18 23:21:26 -0700

    Initial project commit
    This is the ASP.NET Connect sample with a new controller.
    https://github.com/microsoftgraph/aspnet-connect-sample

---
+++ mail-client-ms-graph-aspnet/Controllers/InboxController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Graph;
+using Microsoft_Graph_Mail_Client.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Resources;
+
+namespace Microsoft_Graph_Mail_Client.Controllers
+{
+    public class InboxController : Controller
+    {
+        // GET: Inbox
+        public ActionResult Index()
+        {
+        
+            return View();
+        }
+
+        public async Task<ActionResult> GetInboxMail()
+        {
+            try
+            {
+
+                // Initialize the GraphServiceClient.
+                GraphServiceClient graphClient = SDKHelper.GetAuthenticatedClient();
+
+                // Get the current user. 
+                // This sample only needs the user's email address, so select the mail and userPrincipalName properties.
+                // If the mail property isn't defined, userPrincipalName should map to the email for all account types. 
+                User me = await graphClient.Me.Request().Select("mail,userPrincipalName").GetAsync();
+                ViewBag.Email = me.Mail ?? me.UserPrincipalName;
+                return View("Graph");
+            }
+            catch (ServiceException se)
+            {
+                if (se.Error.Message == Resource.Error_AuthChallengeNeeded) return new EmptyResult();
+                return RedirectToAction("Index", "Error", new { message = Resource.Error_Message + Request.RawUrl + ": " + se.Error.Message });
+            }
+        }
+    }
+}
